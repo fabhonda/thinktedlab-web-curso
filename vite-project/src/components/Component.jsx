67495e9a590a2c87b4af298a3390a8a15a7a1256
Fabrizio Honda <fabrizio.honda10@gmail.com>
2024-07-06 14:41:58 -0400

    Updated files and inserted react components

---
+++ vite-project/src/components/Component.jsx
@@ -0,0 +1,10 @@
+import './Component.css'
+export default function Component(){
+    return(
+    <div>
+        <h1 className='container-h1'>Vamos para o Habib's?</h1>
+        <h2 className='container-h2'>É menos de 30 reais.</h2>
+        
+    </div>
+) 
+}
